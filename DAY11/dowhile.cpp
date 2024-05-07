5b8cc3cf3c13548904786395bc8f8a016a1a18f0
bugslayer29 <rohitprem2003kr@gmail.com>
2024-05-07 21:44:20 +0530

    day11 added

---
+++ DAY11/dowhile.cpp
@@ -0,0 +1,29 @@
+                                                        // // synatax of do-while 
+
+                                                        // initialize;
+                                                        // do
+                                                        // {
+                                                        //     code execution;
+                                                        //     update;
+                                                        // }
+                                                        // while(break)
+// print table of 8;
+#include<iostream>
+using namespace std;
+int main(){
+    
+int n=8;
+int i=1;
+
+do
+{
+   cout << i*n << endl;
+   i=i+1;
+} while (i<=10);
+
+
+
+return 0;
+}
+
+
---
+++ DAY11/whileloop.cpp
@@ -0,0 +1,71 @@
+
+// syntax of while loop
+// intialization;
+// while(condition)
+// {
+//     code execution;
+//     iteration;
+// }
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    
+
+// 1. Print table of n using while loop.
+
+// int n;
+// cout << "Enter the value of n \n";
+// cin >> n;
+// int i = 1;
+// while(i<=10)
+// {
+//     cout << i*n << endl;
+//     i=i+1;
+// }
+
+// 2. calculate the factor of the given number
+// int n;
+// cout << "enter the value of n : \n";
+// cin >> n;
+// int i =1;
+// while(i<=n)
+// {
+//     if(n%i==0)
+//     {
+//         cout << i << endl;
+//     }
+//     i = i+1;
+// }
+
+// // claculate all even number between 1 to n;
+// int n;
+// cout << "enter the value of n : \n";
+// cin >> n;
+// int i =1;
+// while(i<=n)
+// {
+//     if(i%2==0)
+//     {
+//         cout << i << endl;
+//     }
+//     i = i+1;
+// }
+
+// claculate all odd number between 1 to n;
+int n;
+cout << "enter the value of n : \n";
+cin >> n;
+int i =1;
+while(i<=n)
+{
+    if(i%2!=0)
+    {
+        cout << i << endl;
+    }
+    i = i+1;
+}
+
+return 0;
+}
